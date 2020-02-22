c120f04c1cd0ecc34b32736f27fea0c1cb6d8b0d
fashionovic <60850673+fashionovic@users.noreply.github.com>
2020-02-22 18:03:08 +0200

    Delete __init__.py

--- project72/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = ["env", "output_to_xml", "parse_json", "parse_excel"]
-
-from .environment import env
-from .parsers import parse_json
-from .parsers import parse_excel
-from .exporters import output_to_xml
-
