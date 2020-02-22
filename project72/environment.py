97fc37f0bd59a68240f55694d37588f930b5215e
fashionovic <60850673+fashionovic@users.noreply.github.com>
2020-02-22 18:02:57 +0200

    Delete environment.py

--- project72/environment.py
+++
@@ -1,9 +0,0 @@
-from jinja2 import Environment, PackageLoader, select_autoescape
-
-env = Environment(
-    loader=PackageLoader(__name__, "templates"),
-    autoescape=select_autoescape(["xml"]),
-    trim_blocks=True
-)
-
-__all__ = ["env"]
